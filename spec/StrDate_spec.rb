2b74eb7d295085cd53381948657e8f65c3ed5995
tobias <tobiasherzke@openmha.com>
2018-09-29 21:01:32 +0200

    Test that StrDate cannot be constructed with no parameter

--- spec/StrDate_spec.rb
+++ spec/StrDate_spec.rb
@@ -6,6 +6,10 @@ RSpec.describe StrDate  do
     it 'can be created from a string containing 8 digits' do
       expect{StrDate.new("20180929")}.to_not raise_exception
     end
+
+    it 'rejects a constructor without parameter' do
+      expect{StrDate.new}.to raise_exception(ArgumentError)
+    end
   end
 
 end
